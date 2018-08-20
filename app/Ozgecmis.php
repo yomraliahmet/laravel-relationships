a8562f9fd56b8c7bb5bb7e1be4c79381abcef578
Ahmet YILDIRIM <ahmetyildirim@live.com>
2018-08-20 11:15:28 +0300

    Belongs To(Ait Olmak), ilişki örneği.

--- app/Ozgecmis.php
+++ app/Ozgecmis.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Ozgecmis extends Model
 {
     protected $table= "ozgecmis";
+
+
+    // Özgeçmişin ait olduğu kullanıcı bilgilerini çekiyoruz.
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    // Üstteki ile aynı işlemi görür, farklı olarak ilişki kurulan id kolonları da yazılır.
+    /*
+    public function user()
+    {
+        return $this->belongsTo('App\User','user_id','id');
+    }
+    */
 }
+
+
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Kullanıcının özgeçmişine ulaşıyoruz.
 Route::get('/user', function () {
-    $user = App\User::with('ozgecmis')->firstOrFail();
+    $user = App\User::with('ozgecmis')->where('id',2)->firstOrFail();
 
     echo "id : ".$user->id."<br>";
     echo "name : ".$user->name."<br>";
     echo "email : ".$user->email."<br>";
 
 });
+
+// Özgeçmişin ait olduğu kullanıcının bilgilerini çekiyoruz.
+Route::get('/ozgecmis', function(){
+    $ozgecmis = App\Ozgecmis::with('user')->where('id',1)->firstOrFail();
+
+    echo "id : ".$ozgecmis->id."<br>";
+    echo "isim : ".$ozgecmis->isim."<br>";
+    echo "soyisim : ".$ozgecmis->soyisim."<br>";
+    echo "sehir : ".$ozgecmis->sehir."<br>";
+    echo "meslek : ".$ozgecmis->meslek."<br>";
+
+});
+
+
