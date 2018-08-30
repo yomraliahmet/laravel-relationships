96ba31457e64b5d06fb13543e9252fb3ff65b1e5
Ahmet YILDIRIM <ahmetyildirim@live.com>
2018-08-30 10:09:20 +0300

    Accessors - Mutators örnek..

--- app/Tag.php
+++ app/Tag.php
@@ -19,4 +19,16 @@ class Tag extends Model
     {
         return $this->morphedByMany('App\Video', 'taggable');
     }
+
+    // Accessors - Veri çekerken kolon değerleri üzerinde işlem yapmayı sağlar.
+    public function getTitleAttribute($value)
+    {
+        return mb_strtoupper($value, "UTF-8");
+    }
+
+    // Mutators - Veri kaydederken kolon değerleri üzerinde işlem yapmayı sdağlar.
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['name'] = mb_strtoupper($value, "UTF-8");
+    }
 }
